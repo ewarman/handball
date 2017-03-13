1e5480864e93382414ad9cbca3217d93e07a5a9c
Emily Warman <ewarman@covermymeds.com>
2017-03-13 18:08:16 -0400

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the league!"
+      flash[:success] = 'Welcome to the league!'
       redirect_to @user
     else
       render 'new'
